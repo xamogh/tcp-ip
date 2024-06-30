e18ed0af29e03ff1cd3e674b746c520537eaaabc
Amogh <xamoghx@gmail.com>
2024-06-30 21:26:33 +0545

    Add tcp packets filtering

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,32 @@ fn main() -> io::Result<()> {
         let nbytes = iface.recv(&mut buf[..])?;
 
         // https://www.kernel.org/doc/Documentation/networking/tuntap.txt
-        let flag = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // ethernet level protocal and flags through tun
+        let _ether_flag = u16::from_be_bytes([buf[0], buf[1]]);
+        let ether_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
-        match proto {
+        match ether_proto {
             // 800 is ipv4
             0x0800 => {
                 match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
                     Ok(packet) => {
-                        eprintln!(
-                            "Flag: {:x}, Proto: {:x}, Source Addr: {}, Destination Addr: {}",
-                            flag,
-                            proto,
-                            packet.source_addr(),
-                            packet.destination_addr()
-                        )
+                        let source_addr = packet.source_addr();
+                        let destination_addr = packet.destination_addr();
+                        match etherparse::TcpHeaderSlice::from_slice(
+                            &buf[4 + packet.slice().len()..nbytes],
+                        ) {
+                            Ok(packet) => {
+                                eprintln!(
+                                    "Source Addr: {}, Destination Addr: {}, port: {}",
+                                    source_addr,
+                                    destination_addr,
+                                    packet.destination_port()
+                                )
+                            }
+                            Err(_) => {
+                                eprintln!("Ignoring non tcp packet")
+                            }
+                        }
                     }
                     Err(_) => {
                         eprintln!("Ignoring packet (etherparse cant parse)")
