85b7e670e24b60cf7eea80c10df0781becf0876a
Amogh <xamoghx@gmail.com>
2024-06-30 20:56:55 +0545

    Parse ipv4 packets

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,32 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = iface.recv(&mut buf[..])?;
-        eprintln!("{} bytes, data: {:x?}", nbytes, &buf[..nbytes]);
+
+        // https://www.kernel.org/doc/Documentation/networking/tuntap.txt
+        let flag = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+
+        match proto {
+            // 800 is ipv4
+            0x0800 => {
+                match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+                    Ok(packet) => {
+                        eprintln!(
+                            "Flag: {:x}, Proto: {:x}, Source Addr: {}, Destination Addr: {}",
+                            flag,
+                            proto,
+                            packet.source_addr(),
+                            packet.destination_addr()
+                        )
+                    }
+                    Err(_) => {
+                        eprintln!("Ignoring packet (etherparse cant parse)")
+                    }
+                };
+            }
+            _ => {
+                eprintln!("Ignoring non ipv4 packet")
+            }
+        }
     }
 }
